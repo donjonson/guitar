7a177ebab94dbca55dbc3c7b29c585ce3e69441a
donjonson <ltcomanderdata@gmail.com>
2021-08-22 17:36:49 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,37 @@
 music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
-basic.forever(function on_forever() {
+basic.forever(function () {
     while (input.pinIsPressed(TouchPin.P0)) {
         basic.showLeds(`
             . . . . .
-                        . . . . .
-                        . # . . #
-                        . # # # #
-                        . # . # .
-        `)
+            . . . . .
+            . # . . #
+            . # # # #
+            . # . # .
+            `)
         music.playTone(349, music.beat(BeatFraction.Whole))
     }
 })
-basic.forever(function on_forever2() {
+basic.forever(function () {
     while (input.pinIsPressed(TouchPin.P1)) {
         basic.showLeds(`
             . . . . .
-                        . # # . .
-                        . # . . .
-                        . # # # #
-                        . # . # #
-        `)
+            . # # . .
+            . # . . .
+            . # # # #
+            . # . # #
+            `)
         music.playTone(440, music.beat(BeatFraction.Whole))
     }
 })
-basic.forever(function on_forever3() {
+basic.forever(function () {
     while (input.pinIsPressed(TouchPin.P2)) {
         basic.showLeds(`
             . . . . .
-                        . # # . .
-                        . # . . .
-                        . # # # .
-                        . # . . #
-        `)
+            . # # . .
+            . # . . .
+            . # # # .
+            . # . . #
+            `)
         music.playTone(523, music.beat(BeatFraction.Whole))
     }
 })
